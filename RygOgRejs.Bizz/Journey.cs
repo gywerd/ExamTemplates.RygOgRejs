eb9372521d26f01a8681949bafdf6189fdc25622
EmilC-Aspit <31403281+EmilC-Aspit@users.noreply.github.com>
2017-09-25 10:26:08 +0200

    Commented stuff

--- RygOgRejs.Bizz/Journey.cs
+++ RygOgRejs.Bizz/Journey.cs
@@ -8,5 +8,10 @@ namespace RygOgRejs.Bizz
 {
     public class Journey
     {
+        // string macAddress = (from nic in NetworkInterface.GetAllNetworkInterfaces() where nic.OperationalStatus == OperationalStatus.Up select nic.GetPhysicalAddress().ToString()).FirstOrDefault(); 
     }
 }
+
+//server stuff will delete later
+//adgang til database 10.205.44.39,49172 Aspit, Sever2012
+//Data Source=10.205.44.39,49172;Initial Catalog=RygOgRejs;Integrated Security=False;User ID=Aspit;Password=********;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False
