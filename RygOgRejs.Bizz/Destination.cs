58afc9bdfe59cf018b17c8db6655448abf178c06
EmilC-Aspit <31403281+EmilC-Aspit@users.noreply.github.com>
2017-10-11 12:14:39 +0200

    fdfdfd

--- RygOgRejs.Bizz/Destination.cs
+++ RygOgRejs.Bizz/Destination.cs
@@ -11,6 +11,8 @@ namespace RygOgRejs.Bizz.Entities
         #region Fields
         private int destinationId;
         private string destinationName;
+        private float adultPrice;
+        private float childPrice;
         #endregion
 
         #region Constructors
@@ -24,17 +26,40 @@ namespace RygOgRejs.Bizz.Entities
         /// </summary>
         /// <param name="destId"></param>
         /// <param name="destName"></param>
-        public Destination(int destId, string destName)
+        public Destination(int destinationId, string destinationName, float adultaPrice, float childPrice)
         {
-            this.destinationId = destId;
-            this.destinationName = destName;
+            DestinationId = destinationId;
+            DestinationName = destinationName;
+            AdultPrice = adultPrice;
+            ChildPrice = childPrice;
         }
         #endregion
 
 
         #region Properties
-        public int DestinationId { get => destinationId; set => destinationId = value; }
-        public string DestinationName { get => destinationName; set => destinationName = value; }
+        public int DestinationId
+        {
+            get => destinationId;
+            set => destinationId = value;
+        }
+
+        public string DestinationName
+        {
+            get => destinationName;
+            set => destinationName = value;
+        }
+
+        public float AdultPrice
+        {
+            get => adultPrice;
+            set => adultPrice = value;
+        }
+
+        public float ChildPrice
+        {
+            get => childPrice;
+            set => childPrice = value;
+        }
         #endregion
     }
 }
