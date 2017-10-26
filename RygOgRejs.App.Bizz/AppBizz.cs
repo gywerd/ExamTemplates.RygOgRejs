3420303905623a84988fb1c1e5376a7acc899da8
EmilC-Aspit <31403281+EmilC-Aspit@users.noreply.github.com>
2017-10-26 11:40:00 +0200

    Removed region

--- RygOgRejs.App.Bizz/AppBizz.cs
+++ RygOgRejs.App.Bizz/AppBizz.cs
@@ -41,7 +41,6 @@ namespace RygOgRejs.Bizz.App
             List<DestinationList> destinationList = new List<DestinationList>(); //List containing available destinations to be viewed in DataViewJourneys
             #endregion
         #endregion
-
         #region Methods
         /// <summary>
         /// Code that clears temporary journey, payer & transaction fields after completed sale
